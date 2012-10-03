1719d7dcbe76b09279dfe2e0c58e0309daf4cc8c
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-10-03 12:01:09 -0700

    Add util.js into the precompile part

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Pet
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['util.js']
+
   end
 end
