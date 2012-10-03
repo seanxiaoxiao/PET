94b58d3f3edfb49ca89fa5a7ce7d58d89c6e55e0
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-10-03 08:49:39 -0700

    Fix the link in tabs

--- app/controllers/project_phases_controller.rb
+++ app/controllers/project_phases_controller.rb
@@ -2,9 +2,13 @@ class ProjectPhasesController < ApplicationController
 
   respond_to :html
 
+  @active_tab = :phases
+
   def index
     @project = Project.find(params[:project_id])
     respond_with @project
   end
 
+
 end
+
