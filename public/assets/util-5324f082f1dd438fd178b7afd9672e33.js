d9a7f79e16af12b580ee777bd7e0458327aed4ce
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-10-04 08:55:55 -0700

    The show method in project phase controller

---
+++ public/assets/util-5324f082f1dd438fd178b7afd9672e33.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: xiaoxiao
+ * Date: 10/3/12
+ * Time: 11:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+var PET=PET||{};PET.activeTab=function(e){document.getElementById(e).setAttribute("class","active")};
