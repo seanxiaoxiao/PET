0badc784e3a5532e1ca95d1ba205e043a4cfc175
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-10-04 08:56:28 -0700

    Delete the precompiled files

--- public/assets/util-5324f082f1dd438fd178b7afd9672e33.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created with JetBrains RubyMine.
- * User: xiaoxiao
- * Date: 10/3/12
- * Time: 11:05 AM
- * To change this template use File | Settings | File Templates.
- */
-var PET=PET||{};PET.activeTab=function(e){document.getElementById(e).setAttribute("class","active")};
