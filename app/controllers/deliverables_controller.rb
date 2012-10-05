7c4e983e1cc58b604d99d30aaea614b286ccaaae
Sumeet Kumar <kr.sumeet@gmail.com>
2012-10-05 10:50:35 -0700

    Added test for the deliverable controller

--- app/controllers/deliverables_controller.rb
+++ app/controllers/deliverables_controller.rb
@@ -2,13 +2,22 @@ class DeliverablesController < ApplicationController
   respond_to :html
 
   def new
+    @project_phase = ProjectPhase.find(params[:project_phase_id])
     @deliverable = Deliverable.new
     respond_with @deliverable
   end
 
   def create
-    @deliverable = Deliverable.create(params[:deliverable])
-    respond_with @deliverable
+    @deliverable = Deliverable.new(params[:deliverable])
+    @project = Project.find(params[:project_id])
+    @project_phase = ProjectPhase.find(params[:project_phase_id])
+    @deliverable.project_phase= @project_phase
+    @deliverable.save
+    respond_with(@project, @project_phase, @deliverable)
+
   end
 
+  def show
+
+  end
 end
---
+++ spec/controllers/deliverables_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe DeliverablesController do
+
+  let!(:lifecycle) { FactoryGirl.create(:lifecycle) }
+  let!(:lifecycle_phase) { FactoryGirl.create(:lifecycle_phase, :lifecycle => lifecycle) }
+  let!(:project) { FactoryGirl.create(:project, :lifecycle => lifecycle) }
+  let!(:deliverable) { FactoryGirl.create(:deliverable, :project_phase => project.project_phases.first) }
+
+  let!(:complexity) { FactoryGirl.create(:complexity) }
+  let!(:deliverable_type) { FactoryGirl.create(:deliverable_type) }
+  let!(:valid_deliverable_attributes) do
+    {:deliverable => {
+        :name => "Test Title",
+        :description => "Test content",
+        :complexity_id => complexity.id,
+        :deliverable_type_id => deliverable_type.id
+    }}
+  end
+
+  describe "GET new" do
+    it "assigns a new deliverable as Deliverable" do
+      get :new, {:project_id => project.to_param, :project_phase_id => project.project_phases.first.to_param}
+      assigns(:deliverable).should be_a_new(Deliverable)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new Deliverable" do
+        expect {
+          post :create, {:project_id => project.to_param,
+                         :project_phase_id => project.project_phases.first.to_param,
+                         :deliverable => {
+                             :name => "Test Title",
+                             :description => "Test content",
+                             :complexity_id => complexity.id,
+                             :deliverable_type_id => deliverable_type.id
+                         }}
+        }.to change(Deliverable, :count).by(1)
+      end
+    end
+
+    describe "with invalid params" do
+      it "doe not create a new Deliverable" do
+        expect {
+          post :create, {:project_id => project.to_param,
+                         :project_phase_id => project.project_phases.first.to_param,
+                         :deliverable => {
+                             :description => "Test content",
+                             :complexity_id => complexity.id,
+                             :deliverable_type_id => deliverable_type.id
+                         }}
+        }.to change(Deliverable, :count).by(0)
+      end
+    end
+  end
+end
